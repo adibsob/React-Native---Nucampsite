28089ffb3336baec45b72c98f8765c581ec45302
adibsob <99847582+adibsob@users.noreply.github.com>
2022-11-16 23:53:21 -0800

    Gestures Part 1

--- nucampsite/features/campsites/RenderCampsite.js
+++ nucampsite/features/campsites/RenderCampsite.js
@@ -1,16 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, PanResponder, Alert } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { baseUrl } from '../../shared/baseUrl';
 import * as Animatable from 'react-native-animatable';
 
 const RenderCampsite = (props) => {
     const { campsite } = props;
+
+    const isLeftSwipe = ({ dx }) => dx < -200;
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderEnd: (e, gestureState) => {
+            console.log('pan responder end', gestureState);
+            if (isLeftSwipe(gestureState)) {
+                Alert.alert(
+                    'Add Favorite',
+                    'Are you sure you wish to add ' +
+                    campsite.name +
+                    ' to favorites?',
+                    [
+                        {
+                            text: 'Cancel',
+                            style: 'cancel',
+                            onPress: () => console.log('Cancel Pressed')
+                        },
+                        {
+                            text: 'OK',
+                            onPress: () =>
+                                props.isFavorite
+                                    ? console.log('Already set as a favorite')
+                                    : props.markFavorite()
+                        }
+                    ],
+                    { cancelable: false }
+                );
+            }
+        }
+    });
+
     if (campsite) {
         return (
             <Animatable.View
                 animation='fadeInDownBig'
                 duration={2000}
                 delay={1000}
+                {...panResponder.panHandlers}
             >
                 <Card containerStyle={styles.cardContainer}>
                     <Card.Image source={{ uri: baseUrl + campsite.image }}>
@@ -22,8 +56,8 @@ const RenderCampsite = (props) => {
                     <View style={styles.cardRow}>
                         <Icon
                             name={props.isFavorite ? 'heart' : 'heart-o'}
-                            type="font-awesome"
-                            color="#f50"
+                            type='font-awesome'
+                            color='#f50'
                             raised
                             reverse
                             onPress={() =>
@@ -33,12 +67,12 @@ const RenderCampsite = (props) => {
                             }
                         />
                         <Icon
-                            name="pencil"
-                            type="font-awesome"
-                            color="#5637DD"
+                            name='pencil'
+                            type='font-awesome'
+                            color='#5637DD'
                             raised
                             reverse
-                            onPress={() => props.onShowModal()}
+                            onPress={props.onShowModal}
                         />
                     </View>
                 </Card>
@@ -52,23 +86,23 @@ const styles = StyleSheet.create({
     cardContainer: {
         padding: 0,
         margin: 0,
-        marginBottom: 20,
+        marginBottom: 20
     },
     cardRow: {
         alignItems: 'center',
         justifyContent: 'center',
         flex: 1,
         flexDirection: 'row',
-        margin: 20,
+        margin: 20
     },
     cardText: {
-        textShadowColor: 'rgba(0,0,0,1)',
+        textShadowColor: 'rgba(0, 0, 0, 1)',
         textShadowOffset: { width: -1, height: 1 },
         textShadowRadius: 20,
         textAlign: 'center',
         color: 'white',
-        fontSize: 20,
-    },
+        fontSize: 20
+    }
 });
 
 export default RenderCampsite;
